5f1f5525ef666a5ef622691153e80b8c08254c6c
vedantbartakke-25 <vedant.bartakke@gmail.com>
2026-06-13 17:20:59 +0530

    Remove logo and favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,21 +22,6 @@ const Home = () => {
         <div className="absolute inset-0 bg-black/55"></div>
       </motion.div>
 
-      {/* Logo in top-left corner */}
-      <motion.div 
-        className="absolute top-6 left-6 md:top-8 md:left-8 z-20 w-16 h-16 md:w-20 md:h-20"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, delay: 0.2 }}
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.95 }}
-      >
-        <img 
-          src="/logo.png" 
-          alt="TeeGoat Logo" 
-          className="w-full h-full object-contain mix-blend-screen" 
-        />
-      </motion.div>
 
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center text-center px-4">
